dd4dbbb67740d4e2de15c4fa7d88d20e96e4202d
AhmedRoshdy <ahmedroshdy01029@gmail.com>
2021-07-14 14:15:35 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,15 +1,19 @@
 let keyboard = document.querySelector('.piano__keyboard')
+let controls = document.querySelectorAll('.piano__control__option')
 let pianoNotes = ['C', 'D', 'E', 'F', 'G', 'A', 'B']
+let keyMap = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Z', 'X', 'C', 'V', 'B', 'N'];
 
 // initial function 
 let init = () => {
     for (let i = 1; i <= 5; i++) {
         for (let j = 0; j < 7; j++) {
             let key = createKey('white', pianoNotes[j], i)
+            key.dataset.keyboard = keyMap[j + (i - 1) * 7]
             keyboard.appendChild(key);
 
             if (j != 2 && j != 6) {
                 key = createKey('black', pianoNotes[j], i)
+                key.dataset.keyboard = '⇧+' + keyMap[j + (i - 1) * 7]
                 let emptySpace = document.createElement('div')
                 emptySpace.className = 'empty-space'
                 emptySpace.appendChild(key)
@@ -29,6 +33,8 @@ let createKey = (type, note, octave) => {
     key.dataset.letterNote = type == 'white' ? note + octave : note + '#' + octave;
     key.dataset.letterNoteFileName = type == 'white' ? note + octave : note + 's' + octave;
     key.textContent = key.dataset.letterNote
+
+
 
     key.addEventListener('mousedown', () => {
         playSound(key);
